ec19a7c368095e287bc15f9405e12533a46638ed
gaoxiaoming <gxm@heanny.cn>
2018-07-20 15:42:28 +0800

    第一次提交

---
+++ README.md
@@ -0,0 +1,45 @@
+# vue-music
+
+> Vue.js 音乐 App
+
+
+## 项目主要技术栈
+
+vue2.0 vuex node 
+
+
+### 安装与运行
+
+```
+git clone https://github.com/songhaoreact/vue-music.git
+
+cd vue-music
+
+npm install
+
+npm run dev //服务端运行 访问 http://localhost:8080
+
+npm run build 项目打包 
+
+
+
+``` 
+## 已实现功能 
+
+- [x]   图片懒加载
+- [x]   历史搜索
+- [x]   音乐收藏
+- [x]   音乐播放
+- [x]   自动播放下一曲
+- [x]   音乐列表（增删）
+- [x]   下一曲
+- [x]   歌手
+- [x]   歌手详情
+- [x]   排行榜
+- [x]   排行榜详情页
+- [x]   搜索
+- [x]   loading
+- [x]   播放详情页
+- [x]   滚动歌词
+- [x]   展示音乐进度
+- [x]   音乐进度的拖拽
---
+++ src/common/js/jsonp.js
@@ -0,0 +1,32 @@
+import originJsonp from 'jsonp'
+
+
+/*
+* url: 地址
+* data：地址栏查询数据
+* option：查询参数
+* */
+
+export default function jsonp(url, data, option) {
+  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+
+  return new Promise((resolve, reject) => {
+    originJsonp(url, option, (err, data) => {
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(err)
+      }
+    })
+  })
+}
+
+export function param(data) {
+  let url = ''
+  for (var k in data) {
+    let value = data[k] !== undefined ? data[k] : ''
+    url += '&' + k + '=' + encodeURIComponent(value)
+  }
+  return url ? url.substring(1) : ''
+}
+
